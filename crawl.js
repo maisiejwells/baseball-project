2ae309f117e49003324ed8409789b54c4cbb1be2
Maisie Wells <maisiejwells@gmail.com>
2019-04-19 19:50:57 -0600

    added crawl page

---
+++ crawl.js
@@ -0,0 +1,21 @@
+import { films } from './starwars/films.js'
+
+const intro = document.querySelector('.starwars')
+
+films.sort((a,b) => (a.episode_id > b.episode_id) ? 1 : -1 )
+
+films.forEach(film => {
+  let tile = document.createElement('div')
+tile.className = "tile"
+  let titleElement = document.createElement('h1')
+  tile.appendChild(titleElement)
+  titleElement.textContent = film.title
+
+
+  let crawlElement = document.createElement('div')
+  crawlElement.className = "crawl"
+  tile.appendChild(crawlElement)
+  crawlElement.textContent = film.opening_crawl
+
+  intro.appendChild(tile)
+})
